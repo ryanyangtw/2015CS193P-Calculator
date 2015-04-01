9c4090fe62c952c4f5e65e84e0c7c6572786433c
Ryan <hiphubryan@gmail.com>
2015-04-01 14:33:51 +0800

    Finish class5

--- Calculator/CalculatorBrain.swift
+++ Calculator/CalculatorBrain.swift
@@ -70,6 +70,37 @@ class CalculatorBrain
     
   }
   
+  
+  typealias PropertyList = AnyObject
+  var program: PropertyList { //guaranteed to be a PropertyList
+    get {
+      return opStack.map { $0.description }
+      
+      /*
+      var returnValue = Array<String>()
+      for op in opStack {
+        returnValue.append(op.description)
+      }
+      return returnValue
+      */
+    }
+    set {
+      if let opSymbols = newValue as? Array<String> {
+        var newOpStack = [Op]()
+        for opSymbol in opSymbols {
+          if let op = knownOps[opSymbol] {
+            newOpStack.append(op)
+          } else if let operand =
+            NSNumberFormatter().numberFromString(opSymbol)?.doubleValue {
+            newOpStack.append(.Operand(operand))
+          }
+          opStack = newOpStack
+        }
+        
+      }
+    }
+  }
+  
   // There is an implicit let in front of all things you pass (read-only)
   private func evaluate(ops: [Op]) -> (result: Double?, remainingOps: [Op]) {
     if !ops.isEmpty {
